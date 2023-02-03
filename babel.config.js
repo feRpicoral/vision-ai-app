d82ec8802397675dabb89730b1b5d8c3a827fda9
Fernando Picoral <fernandopicoral@gmail.com>
2023-02-03 00:04:11 -0700

    feat: initial commit

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
+const tsconfig = require('./tsconfig.json');
+const paths = tsconfig.compilerOptions.paths;
+const alias = {};
+
+for (const [key, value] of Object.entries(paths)) {
+    alias[key.replace('/*', '')] = value.map(str => str.replace('/*', ''));
+}
+
+// eslint-disable-next-line no-undef
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['./'],
+                    extensions: [
+                        '.ios.js',
+                        '.android.js',
+                        '.js',
+                        '.ts',
+                        '.tsx',
+                        '.json'
+                    ],
+                    alias
+                }
+            ]
+        ]
+    };
+};
---
+++ metro.config.js
@@ -0,0 +1,3 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getDefaultConfig } = require('expo/metro-config');
+module.exports = getDefaultConfig(__dirname);
